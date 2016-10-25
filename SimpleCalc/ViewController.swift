a66067472df331c90fcf9a61ce510d3061b85e13
Keegan Farley <keegomyneego@live.com>
2016-10-25 16:30:45 -0700

    wow added a ton of logic for dealing with operators and expressions, build compiles so im committing

--- SimpleCalc/ViewController.swift
+++ SimpleCalc/ViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-extension Array {
-
-}
-
 class ViewController: UIViewController {
 
+    /// UI
     @IBOutlet weak var expressionQueueLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+
+    /// Operators
+    var definedOperators: [String : Operator] = [
+        "+" : BinaryOperator("+", +),
+        "-" : BinaryOperator("-", -),
+        "×" : BinaryOperator("x", *),
+        "÷" : BinaryOperator("÷", /),
+        "﹪" : BinaryOperator("%", %),
+        "n!" : UnaryOperator("!", factorial),
+        "#" : MultaryOperator("count", count),
+        "x̅" : MultaryOperator("avg", average)
+    ]
 
     // Tightly couple this with total label's text.
     // Conversion to number can happen at evaluation time
@@ -28,8 +37,7 @@ class ViewController: UIViewController {
         }
     }
 
-    // Tightly couple this with total label's text.
-    // Conversion to number can happen at evaluation time
+    // Tightly couple this with expression queue label's text.
     private var expressionQueue: [Expression] = [] {
         willSet {
             expressionQueueLabel.text = newValue
@@ -63,8 +71,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func operatorSelected(_ sender: UIButton) {
-        let numberExpression = Expression(string: currentNumber)
-        let operatorExpression = Expression(string: sender.currentTitle ?? "")
+        let numberExpression: Expression = Number(currentNumber)
+        let operatorExpression: Expression = definedOperators[sender.currentTitle!]!
 
         expressionQueue += [numberExpression, operatorExpression]
 
@@ -90,11 +98,99 @@ class ViewController: UIViewController {
     }
 }
 
-class Expression : CustomStringConvertible {
+
+// Other classes I had to shove into this file because this assignment
+// requires us to only modify ViewController.swift and Main.storyboard...
+
+typealias NumberType = Int
+
+//------------------------------------------------------------
+// Expressions
+//------------------------------------------------------------
+
+protocol Expression : CustomStringConvertible {}
+
+class Number : Expression {
+
+    static let defaultNumberValue: NumberType = 0
+
     let description: String
 
-    init(string: String) {
-        self.description = string
+    init(_ symbol: String) {
+        self.description = symbol
+    }
+
+    func getValue() -> NumberType {
+        return NumberType(self.description) ?? {
+            print("Number :: Failed to convert '\(self)' to \(NumberType.self), falling back to default")
+            return Number.defaultNumberValue
+        }()
     }
 }
 
+//------------------------------------------------------------
+// Operators
+//------------------------------------------------------------
+
+typealias OperatorFunction = ([NumberType]) -> NumberType
+
+protocol Operator : Expression {
+    var operate: OperatorFunction { get }
+    var isValidOperandCount: (Int) -> Bool { get }
+}
+
+class UnaryOperator : Operator {
+    let description: String
+    let operate: OperatorFunction
+    let isValidOperandCount: (Int) -> Bool = { $0 == 1 }
+
+    init(_ symbol: String, _ operation: @escaping (NumberType) -> NumberType) {
+        self.description = symbol
+        self.operate = {
+            return operation( $0[$0.count - 1] )
+        }
+    }
+}
+
+class BinaryOperator : Operator {
+    let description: String
+    let operate: OperatorFunction
+    let isValidOperandCount: (Int) -> Bool = { $0 == 2 }
+
+    required init(_ symbol: String, _ operation: @escaping (NumberType, NumberType) -> NumberType) {
+        self.description = symbol
+        self.operate = {
+            return operation( $0[$0.count - 2], $0[$0.count - 1] )
+        }
+    }
+}
+
+class MultaryOperator : Operator {
+    let description: String
+    let operate: OperatorFunction
+    let isValidOperandCount: (Int) -> Bool = { $0 >= 2 }
+
+    required init(_ symbol: String, _ operation: @escaping ([NumberType]) -> NumberType) {
+        self.description = symbol
+        self.operate = operation
+    }
+}
+
+//------------------------------------------------------------
+// Yay global functions :/
+//------------------------------------------------------------
+
+func factorial(n: NumberType) -> NumberType {
+    return n <= 1
+        ? 1
+        : n * factorial(n: n - 1)
+}
+
+func count(nums: [NumberType]) -> NumberType {
+    return NumberType(nums.count)
+}
+
+func average(nums: [NumberType]) -> NumberType {
+    // return sum / count
+    return NumberType(nums.reduce(0, +)) / NumberType(nums.count)
+}
