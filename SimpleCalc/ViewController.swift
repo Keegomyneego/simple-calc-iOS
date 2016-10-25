e6f88247d949dc25328b548d160383d6c0ecb218
Keegan Farley <keegomyneego@live.com>
2016-10-25 12:21:02 -0700

    hooked up buttons to IBAction handlers and got some primitive logic going

--- SimpleCalc/ViewController.swift
+++ SimpleCalc/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expressionQueueLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+
+    // Tightly couple this with total label's text.
+    // Conversion to number can happen at evaluation time
+    private var currentNumber: String {
+        get {
+            return totalLabel.text!
+        }
+        set {
+            totalLabel.text = newValue
+        }
+    }
+
+    // Tightly couple this with total label's text.
+    // Conversion to number can happen at evaluation time
+    private var expressionQueue: [Expression] = [] {
+        willSet {
+            print("willSet to \(newValue)")
+        }
+        didSet {
+            print("didSet to \(oldValue)")
+        }
+    }
+
+    //------------------------------------------------------------
+    // UIViewController Overrides
+    //------------------------------------------------------------
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        resetCurrentNumber()
+        resetExpressionQueue()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +51,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //------------------------------------------------------------
+    // IBActions
+    //------------------------------------------------------------
 
+    @IBAction func numberSelected(_ sender: UIButton) {
+        currentNumber += sender.currentTitle ?? ""
+    }
+
+    @IBAction func operatorSelected(_ sender: UIButton) {
+        let numberExpression = Expression(string: currentNumber)
+        let operatorExpression = Expression(string: sender.currentTitle ?? "")
+
+        expressionQueue += [numberExpression, operatorExpression]
+
+        resetCurrentNumber()
+    }
+
+    @IBAction func backspaceSelected(_ sender: UIButton) {
+    }
+
+    @IBAction func equalsSelected(_ sender: UIButton) {
+    }
+
+    //------------------------------------------------------------
+    // Helper Methods
+    //------------------------------------------------------------
+
+    private func resetCurrentNumber() {
+        currentNumber = ""
+    }
+
+    private func resetExpressionQueue() {
+        expressionQueue = []
+    }
+}
+
+class Expression : CustomStringConvertible {
+    let description: String
+
+    init(string: String) {
+        self.description = string
+    }
 }
 
